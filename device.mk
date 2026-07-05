6430cc209d54a04281999a4a14358802fcb61c58
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-06 01:36:01 +0300

    Revert "LH7n: Switch to common MTK USB gadget init"
    This reverts commit f659b032118fc7de2b677f98a374fb07098d3ff4.

--- device.mk
+++ device.mk
@@ -244,6 +244,7 @@ PRODUCT_PACKAGES += \
     init.modem.rc \
     init.mt6789.power.rc \
     init.mt6789.rc \
+    init.mt6789.usb.rc \
     init.mtkgki.rc \
     init.project.rc \
     init.recovery.usb.rc \
@@ -511,8 +512,7 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set_bool,android_hardware_mediatek_usb,audio_accessory_supported,true)
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
-    android.hardware.usb.gadget-service.mediatek \
-    init.mt6789.usb.rc
+    android.hardware.usb.gadget-service.mediatek
     
 # userdata
 PRODUCT_FS_COMPRESSION := 1
