891423c0a7b8c3636f502bc39e6c6827065728ed
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-05 03:52:28 +0300

    Revert "LH7n: Include JamesDSP"
    This reverts commit 5140b8d27a031fa4c1e700bee68d7c130f9fe0c6.

--- device.mk
+++ device.mk
@@ -247,9 +247,6 @@ PRODUCT_PACKAGES += \
     init.sensor_2_0.rc \
     ueventd.mt6789.rc
 
-# JamesDSP
-$(call inherit-product-if-exists, vendor/JamesDSP/config.mk)
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.millennium
